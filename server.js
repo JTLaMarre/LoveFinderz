59331a5a1cea2b4c9639c69f617ecdd5036d2c08
Jacob LaMarre <lordaufchaos@yahoo.com>
2021-04-23 10:26:15 -0400

    oops on static file path

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/BAC'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/BAC/index.html'));
+res.sendFile(path.join(__dirname+'/dist/Lovefinderz/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
